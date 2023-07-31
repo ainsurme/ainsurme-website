f55692a70510f5a2d91f85ba2f5aa5adfc8ca044
Qian Cheng <91555901+qcheng-dev@users.noreply.github.com>
2023-07-31 15:45:55 -0700

    Update Company.jsx

--- src/containers/company/Company.jsx
+++ src/containers/company/Company.jsx
@@ -16,14 +16,14 @@ const Company = () => (
             <div class = "team-box-text">
                 <img src={ProfileQNC} alt="Nick Q. Cheng"/>
                 <h2>Nick Cheng</h2>
-                <h3>Co-founder</h3>
+                <h3>Founder</h3>
                 <p>Ph.D. in Computer Science from University of Illinois at Urbana-Champaign, 
                     AI expert, lead scientist and engineer. Ex. AWS, Microsoft, Salesforce AI Labs.</p>
             </div>
             <div class = "team-box-text">
                 <img src={ProfileKC} alt="Kane Carpenter"/>
                 <h2>Kane Carpenter</h2>
-                <h3>CMO</h3>
+                <h3>Founding Member</h3>
                 <p>MBA from University of Chicago Booth School of Business, practice lead in organization development strategies,
                     expert in cutomer acquistion, lead generation, and top-line growth.
                 </p>
@@ -34,7 +34,7 @@ const Company = () => (
             <div class = "team-box-text">
                 <img src={ProfileYDH} alt="Donovan Hou"/>
                 <h2>Donovan Hou</h2>
-                <h3>Co-founder</h3>
+                <h3>Founder</h3>
                 <p>MBA from University of Chicago Booth School of Business, credentialed actuary, annuity expert in product design, pricing, and risk management</p>
             </div>
             <div class = "team-box-text">
